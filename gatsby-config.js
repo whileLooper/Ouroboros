b1ff78cb9d9e3d4c0e513fa8b921b55e816dba41
Bo Chen <bochentheone@gmail.com>
2019-07-18 16:46:30 -0400

    Added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ module.exports = {
   siteMetadata: {
     title: 'Mild Souls Studio',
     description: 'Mild Souls studio 网页设计 网站制作 社交媒体 广告投放 业务推广  数据分析 ',
-    siteUrl: 'https://focused-spence-355862.netlify.com/',
-    author: '6strings',
+    siteUrl: 'https://mildsouls.com/',
+    author: 'whilelooper',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,6 +27,32 @@ module.exports = {
         theme_color: `#68008C`,
         display: `standalone`,
         icon: `${__dirname}/src/images/mdb-react-small.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-64802678-5",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Enables Google Optimize Experiment ID
+        // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        // Set Variation ID. 0 for original 1,2,3....
+        // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // Any additional create only fields (optional)
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "mildsouls.com",
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
