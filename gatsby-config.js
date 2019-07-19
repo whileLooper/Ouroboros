05f47cdc6912f859805b6e85041020da5eeb6c80
Bo Chen <bochentheone@gmail.com>
2019-07-18 22:02:24 -0400

    Updated google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,15 +34,15 @@ module.exports = {
       options: {
         trackingId: "UA-64802678-5",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
+        // head: false,
         // Setting this parameter is optional
-        anonymize: true,
+        // anonymize: true,
         // Setting this parameter is also optional
-        respectDNT: true,
+        // respectDNT: true,
         // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
         // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
+        // pageTransitionDelay: 0,
         // Enables Google Optimize using your container Id
         // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
         // Enables Google Optimize Experiment ID
@@ -50,9 +50,9 @@ module.exports = {
         // Set Variation ID. 0 for original 1,2,3....
         // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
         // Any additional create only fields (optional)
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "mildsouls.com",
+        // sampleRate: 5,
+        // siteSpeedSampleRate: 10,
+        // cookieDomain: "mildsouls.com",
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
